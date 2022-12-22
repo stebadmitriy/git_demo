50b2554fa4b7b90297049e79c7f1d0f76e7b6002
stebadmitriy <stebadmitriy@gmail.com>
2022-12-22 17:55:41 +0100

    Added text "I am developer"

--- src/main/java/by/it/academy/printing/PrintService.java
+++ src/main/java/by/it/academy/printing/PrintService.java
@@ -2,6 +2,6 @@ package by.it.academy.printing;
 
 public class PrintService {
 	public void print() {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! I am developer");
 	}
 }
