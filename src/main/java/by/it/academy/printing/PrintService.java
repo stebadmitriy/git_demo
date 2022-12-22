81a29c37106206597da597fdeaf6c717638a2b64
stebadmitriy <stebadmitriy@gmail.com>
2022-12-22 18:27:37 +0100

    Add text "My name is Dmitiry"

--- src/main/java/by/it/academy/printing/PrintService.java
+++ src/main/java/by/it/academy/printing/PrintService.java
@@ -2,6 +2,6 @@ package by.it.academy.printing;
 
 public class PrintService {
 	public void print() {
-		System.out.println("Hello World! I am developer. I am from Belarus");
+		System.out.println("Hello World! I am developer. I am from Belarus. My name is Dmitiry");
 	}
 }
